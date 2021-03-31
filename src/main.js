1f7da2727faae5102057fd73978bb742d4a151b0
Mollycodd1e <selik7@mail.ru>
2021-03-31 15:13:05 +0300

    Добавление блоков в разметку

--- src/main.js
+++ src/main.js
@@ -1,10 +1,33 @@
 import {createSiteMenuTemplate} from './view/site-menu.js';
+import {createSiteInfoTemplate} from './view/site-info.js';
+import {createSiteFilterTemplate} from './view/site-filter.js';
+import {createSiteSortTemplate} from './view/site-sort.js';
+import {createSiteListTemplate} from './view/site-list.js';
+import {createSiteEventTemplate} from './view/site-event.js';
+
+const EVENT_COUNT = 3;
 
 const render = function(container, template, place) {
   container.insertAdjacentHTML(place, template);
 };
 
-const siteMainElement = document.querySelector('.trip-main');
-const siteMenuElement = siteMainElement.querySelector('.trip-controls__navigation');
+const siteHeaderElement = document.querySelector('.page-header');
+const siteTripElement = siteHeaderElement.querySelector('.trip-main');
+const siteNavigationElement = siteTripElement.querySelector('.trip-controls__navigation');
+const siteFilterElement = siteTripElement.querySelector('.trip-controls__filters');
 
-render(siteMenuElement, createSiteMenuTemplate(), 'beforeend');
+render(siteTripElement, createSiteInfoTemplate(), 'afterbegin');
+render(siteNavigationElement, createSiteMenuTemplate(), 'beforeend');
+render(siteFilterElement, createSiteFilterTemplate(), 'beforeend');
+
+const siteMainElement = document.querySelector('.page-body__page-main');
+const siteEventElement = siteMainElement.querySelector('.trip-events');
+
+render(siteEventElement, createSiteSortTemplate(), 'beforeend');
+render(siteEventElement, createSiteListTemplate(), 'beforeend');
+
+const siteListElement = siteEventElement.querySelector('.trip-events__list');
+
+for (let i = 0; i < EVENT_COUNT; i ++) {
+  render(siteListElement, createSiteEventTemplate(), 'beforeend');
+}
