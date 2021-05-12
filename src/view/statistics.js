3a99203880d6c33f9332431ca8364c2d884c5962
Mollycodd1e <selik7@mail.ru>
2021-05-12 21:54:12 +0300

    Добавляет форматирование в статистику

--- src/view/statistics.js
+++ src/view/statistics.js
@@ -183,21 +183,21 @@ const renderTypeChart = (typeCtx, waypoints) => {
 const renderTimeSpendChart = (timeSpendCtx, waypoints) => {
   timeSpendCtx.height = BAR_HEIGHT * 10;
 
-  //function ConvertMinutes(num) {
-  //  const hours = Math.floor(num / 60);
-  //  const days = Math.floor(hours / 24);
-  //  const rhours = hours - days * 24;
-  //  const minutes = Math.floor(num % 60);
+  function ConvertMinutes(num) {
+    const hours = Math.floor(num / 60);
+    const days = Math.floor(hours / 24);
+    const rhours = hours - days * 24;
+    const minutes = Math.floor(num % 60)
+    const dateObj = {
+      D: days < 10 ? '0' + days : days,
+      H: rhours < 10 ? '0' + rhours : rhours,
+      M: minutes < 10 ? '0' + minutes : minutes,
+    }
+    return Object.keys(dateObj).map((item) =>
+      dateObj[item] > 0 ? dateObj[item] + item : ' ').join(' ').trim();
+  }
 
-  //  const dateObj = {
-  //    D: days < 10 ? '0' + days : days,
-  //    H: rhours < 10 ? '0' + rhours : rhours,
-  //    M: minutes < 10 ? '0' + minutes : minutes,
-  //  };
-
-  //  return Object.keys(dateObj).map((item) =>
-  //    dateObj[item] > 0 ? dateObj[item] + item : ' ').join(' ').trim();
-  //}
+  console.log(ConvertMinutes(125))
 
   const timeDifference = (elements) => {
     let sumOfTime = 0;
@@ -243,7 +243,7 @@ const renderTimeSpendChart = (timeSpendCtx, waypoints) => {
           color: '#000000',
           anchor: 'end',
           align: 'start',
-          formatter: (arrayOfTime) => arrayOfTime,
+          formatter: (arrayOfTime) => ConvertMinutes(arrayOfTime),
         },
       },
       title: {
